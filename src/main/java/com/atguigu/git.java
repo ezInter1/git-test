4a226bf5d6662b4f85516cc0904c982124fcac8a
jhy <1079357647@qq.com>
2022-04-22 20:33:52 +0800

    firth

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -1,11 +1,11 @@
 package com.atguigu;
 
 /**
- * @author Gold
- * @create 2022-04-22 20:10
+ *
  */
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello!git");
+        System.out.println("hello!git1");
+        System.out.println("hello!git2");
     }
 }
